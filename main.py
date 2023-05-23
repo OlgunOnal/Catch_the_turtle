b01617609377f3b2b486d73fecfdf8558eb47fbb
OLGUN ONAL <olgunonalgit@gmail.com>
2023-05-23 20:36:59 +0200

    1

---
+++ main.py
@@ -0,0 +1,95 @@
+import random
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("light blue")
+screen.title("Catch The Turtle")
+FONT = ("Arial", 30, "normal")
+grid_size = 12
+score = 0
+game_over = False
+
+
+#turtle list
+turtle_list = []
+
+#score turtle
+score_turtle = turtle.Turtle()
+
+#countdown turtles
+countdown_turtle = turtle.Turtle()
+
+#make turtle (properties == Ozellik)
+x_coordinates = [25,15,0,-15,-25]
+y_coordinates = [20,10,0,-10,-20]
+
+def setup_score_turtle():
+    score_turtle.hideturtle()
+    score_turtle.color("dark blue")
+    score_turtle.penup()
+
+    top_height = screen.window_height() / 2
+    x = top_height * -0.6
+    y = top_height * 0.83
+    score_turtle.setposition(x, y)
+    score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
+def make_turtle(x, y):
+    t = turtle.Turtle()
+
+    def handle_click(x,y):
+        global score
+        score += 1
+        score_turtle.clear()
+        score_turtle.write(arg=f"Score: {score}", move=False, align="center", font=FONT)
+       #print(x,y)
+
+    t.onclick(handle_click)
+    t.penup()
+    t.shape("turtle")
+    t.shapesize(2,2)
+    t.color("dark green")
+    t.goto(x * grid_size, y * grid_size)
+    turtle_list.append(t)
+def setup_turtles():
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x,y)
+def hide_turtles():
+    for t in turtle_list:
+        t.hideturtle()
+def show_turtles_randomly():
+    #Bu kod ayni zamanda bu koda esittir "if not game_over"
+    if game_over == False:
+        hide_turtles()
+        random.choice(turtle_list).showturtle()
+        screen.ontimer(show_turtles_randomly, 500)
+def countdown(time):
+    global game_over
+    countdown_turtle.hideturtle()
+    countdown_turtle.color("green")
+    countdown_turtle.penup()
+    top_height = screen.window_height() / 2
+    x = top_height * 0.6
+    y = top_height * 0.83
+    countdown_turtle.setposition(x, y)
+    countdown_turtle.clear()
+    if time > 0:
+        countdown_turtle.clear()
+        countdown_turtle.write(arg=f"Time: {time}", move=False, align="center", font=FONT)
+        screen.ontimer(lambda: countdown(time - 1), 1000)
+    else:
+        game_over = True
+        countdown_turtle.clear()
+        hide_turtles()
+        countdown_turtle.write(arg="Time: Game Over!", move=False, align="center", font=FONT)
+def start_game_up():
+    turtle.tracer(0)
+    setup_score_turtle()
+    setup_turtles()
+    hide_turtles()
+    show_turtles_randomly()
+    countdown(10)
+    turtle.tracer(1)
+
+start_game_up()
+turtle.mainloop()
